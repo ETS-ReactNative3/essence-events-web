8239a39749ba252d4372ff57d783247344044b0e
Isaiah Nields <imnields@gmail.com>
2019-03-11 21:51:15 -0400

    Added User

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 const userModel = require('../db/user');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
 
-// log
+// log in a user
 function logIn(req, res, next) {
-
+  return res.send(req.body);
 }
+
+
+function createAccount(req, res, next) {
+  return res.send(req.body);
+}
+
+router.post('/create', createAccount);
+router.post('/login', logIn);
+
+module.exports = router;
--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,12 @@ const config = require('./config');
 
 const app = express();
 
+const userRouter = require('./api/user');
+
 mongoose.connect(config.URI, { useNewUrlParser: true });
 
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use('/api/user', userRouter);
+
 app.use(express.static('dist'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
