0ceeb73995f9a9ed11efc0ca58bdb162ebc484be
Isaiah Nields <imnields@gmail.com>
2019-03-28 01:08:51 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -7,10 +7,10 @@ const app = express();
 
 const userRouter = require('./api/user');
 
-mongoose.connect(config.URI, { useNewUrlParser: true });
+mongoose.connect(config.URI, { useNewUrlParser: true, useCreateIndex: true });
 
 app.use('/api/user', userRouter);
 
-app.use(express.static('dist'));
+app.all('*', express.static('dist'));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
