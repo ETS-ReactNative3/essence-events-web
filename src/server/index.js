bd220a2c4d53715745e83ba8d5fdeca6fcfb0ec0
Isaiah Nields <imnields@gmail.com>
2019-03-28 11:07:22 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const os = require('os');
 const config = require('./config');
+const path = require('path');
 
 const app = express();
 
@@ -13,6 +14,6 @@ app.use(express.static('dist'));
 
 app.use('/api/user', userRouter);
 
-app.use('/*', (req, res) => {res.send('test')});
+app.use('/*', (req, res) => { res.sendFile(path.join(__dirname, '../.././dist/index.html'))});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
