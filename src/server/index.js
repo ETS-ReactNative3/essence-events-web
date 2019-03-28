408818bbafe552297fd8c7e8e04b4652e8f42008
Isaiah Nields <imnields@gmail.com>
2019-03-28 11:28:11 -0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,10 @@ const path = require('path');
 
 const app = express();
 
+const calendarRouter = require('./api/calendar');
+const cartRouter = require('./api/cart');
+const marketplaceRouter = require('./api/marketplace');
+const todoRouter = require('./api/todo');
 const userRouter = require('./api/user');
 
 mongoose.connect(config.URI, { useNewUrlParser: true, useCreateIndex: true });
@@ -14,6 +18,6 @@ app.use(express.static('dist'));
 
 app.use('/api/user', userRouter);
 
-app.use('/*', (req, res) => { res.sendFile(path.join(__dirname, '../.././dist/index.html'))});
+app.use('/*', (req, res) => { res.sendFile(path.join(__dirname, '../../dist/index.html'))});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
